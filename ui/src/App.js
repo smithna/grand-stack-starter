294d2b7132c0425749bb6dc7ffd82b5d98e31113
Nathan Smith <nsmith_piano@hotmail.com>
2019-09-09 08:53:27 -0500

    reorder components

--- ui/src/App.js
+++ ui/src/App.js
@@ -99,14 +99,14 @@ class App extends Component {
           <h2 className='App-headline'>Meetup Mixer</h2>
         </div>
         <div id="wrapper">
-          <div id="personToTopic">
-            <BipartiteGraph data={final_data} size={[800,500]} orientation={"vertical"} />
-          </div>
           <div id="topicToTopic">
-            <ForceGraph data={t2t_data} size={[500,500]} />
+            <ForceGraph data={t2t_data} size={[300,500]} />
+          </div>
+          <div id="personToTopic">
+            <BipartiteGraph data={final_data} size={[300,500]} orientation={"vertical"} />
           </div>
           <div id="personToPerson">
-            <ForceGraph data={p2p_data} size={[500,500]} />
+            <ForceGraph data={p2p_data} size={[300,500]} />
           </div>
         </div>
       </div>

c1392f8fed5944384cc0609d2502dbfaab47d6a9
Nathan Smith <nsmith_piano@hotmail.com>
2019-09-15 21:23:33 -0500

    Change to multipage layout

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,133 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+
 import "./App.css";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  CssBaseline,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/core";
+
+import {
+  Menu as MenuIcon,
+  ChevronLeft as ChevronLeftIcon,
+  Dashboard as DashboardIcon,
+  EventNote as EventNoteIcon,
+  People as PeopleIcon
+} from "@material-ui/icons";
+
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import CommonTopicGraphs from "./CommonTopicGraphs";
 import UserList from "./UserList";
 import { group, rollups } from "d3-array";
+import classNames from "classnames";
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    backgroundColor: "#383838"
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing.unit * 9
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    height: "100vh",
+    overflow: "auto"
+  }
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedView: "Home",
+      open: true
+    };
+  }
+
+  setSelectedView(viewName) {
+    this.setState({
+      selectedView: viewName
+    });
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
       <Query
         query={gql`
@@ -138,19 +258,104 @@ class App extends Component {
           var t2t_data = { nodes: topics, links: t2t_links };
 
           return (
-            <div className="App">
-              <div className="App-header">
-                <h2 className="App-headline">Meetup Mixer</h2>
+            <React.Fragment>
+              <CssBaseline />
+              <div className={classes.root}>
+                <AppBar
+                  position="absolute"
+                  className={classNames(
+                    classes.appBar,
+                    this.state.open && classes.appBarShift
+                  )}
+                >
+                  <Toolbar
+                    disableGutters={!this.state.open}
+                    className={classes.toolbar}
+                  >
+                    <IconButton
+                      color="inherit"
+                      aria-label="Open drawer"
+                      onClick={this.handleDrawerOpen}
+                      className={classNames(
+                        classes.menuButton,
+                        this.state.open && classes.menuButtonHidden
+                      )}
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                    <Typography
+                      component="h1"
+                      variant="title"
+                      color="inherit"
+                      noWrap
+                      className={classes.title}
+                    >
+                      Welcome To GRANDstack
+                    </Typography>
+                  </Toolbar>
+                </AppBar>
+                <Drawer
+                  variant="permanent"
+                  classes={{
+                    paper: classNames(
+                      classes.drawerPaper,
+                      !this.state.open && classes.drawerPaperClose
+                    )
+                  }}
+                  open={this.state.open}
+                >
+                  <div className={classes.toolbarIcon}>
+                    <IconButton onClick={this.handleDrawerClose}>
+                      <ChevronLeftIcon />
+                    </IconButton>
+                  </div>
+                  <Divider />
+                  <List>
+                    <div>
+                      <ListItem
+                        button
+                        onClick={() => this.setSelectedView("Home")}
+                      >
+                        <ListItemIcon>
+                          <DashboardIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                      </ListItem>
+
+                      <ListItem
+                        button
+                        onClick={() => this.setSelectedView("Users")}
+                      >
+                        <ListItemIcon>
+                          <PeopleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Users" />
+                      </ListItem>
+                    </div>
+                  </List>
+                </Drawer>
+                <main className={classes.content}>
+                  <div className={classes.appBarSpacer} />
+
+                  {/* FIXME: Use proper routing here instead  */}
+                  <Typography
+                    component="div"
+                    className={classes.chartContainer}
+                  >
+                    {this.state.selectedView === "Home" ? (
+                      <CommonTopicGraphs
+                        p2p_data={p2p_data}
+                        t2t_data={t2t_data}
+                        bipartite_data={bipartite_data}
+                      />
+                    ) : null}
+                    {this.state.selectedView === "Users" ? (
+                      <UserList data={p2p_data} />
+                    ) : null}
+                  </Typography>
+                </main>
               </div>
-              <CommonTopicGraphs
-                p2p_data={p2p_data}
-                t2t_data={t2t_data}
-                bipartite_data={bipartite_data}
-              />
-              <div id="individual-report">
-                <UserList data={p2p_data} />
-              </div>
-            </div>
+            </React.Fragment>
           );
         }}
       </Query>
@@ -158,4 +363,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);

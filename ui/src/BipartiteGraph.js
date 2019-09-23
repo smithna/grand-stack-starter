ebc5ad5e14bca24333a54619dad3e2819451cbba
Nathan Smith <nsmith_piano@hotmail.com>
2019-09-23 11:18:54 -0500

    consolidate graph files

--- ui/src/BipartiteGraph.js
+++ ui/src/BipartiteGraph.js
@@ -1,81 +1,86 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 import {
   forceSimulation,
   forceCollide,
   forceLink,
-  forceCenter
+  forceCenter,
+  forceX,
+  forceY,
+  forceManyBody
 } from "d3-force";
 import { select, selectAll } from "d3-selection";
 import { transition } from "d3-transition";
 
-class BipartiteGraph extends Component {
-  constructor(props) {
-    super(props);
-    this.createBipartiteGraph = this.createBipartiteGraph.bind(this);
-  }
+function BipartiteGraph(props) {
+  const d3Container = useRef(null);
+  useEffect(() => restart());
+  useEffect(() => restart(), [props.data, d3Container.current]);
 
-  componentDidMount() {
-    this.createBipartiteGraph();
-  }
+  const linkData = props.data.links;
+  const links = linkData.map(d => Object.create(d));
+  const nodes = props.data.nodes.map(d => Object.create(d));
 
-  componentDidUpdate() {
-    this.createBipartiteGraph();
-  }
+  function createLinkedByIndex(links) {
+    let linkedByIndex = {};
 
-  createBipartiteGraph() {
-    const node = this.node;
-    let linkData = this.props.data.links;
-    const links = linkData.map(d => Object.create(d));
-    const nodes = this.props.data.nodes.map(d => Object.create(d));
-    const orientation = this.props.orientation;
-    const displaySize = this.props.size;
-
-    function createLinkedByIndex(links) {
-      let linkedByIndex = {};
-
-      links.forEach(d => {
-        linkedByIndex[[d.source, d.target]] = 1;
+    links.forEach(d => {
+      linkedByIndex[[d.source, d.target]] = 1;
+      if (props.layout === "bipartite") {
         links.forEach(i => {
           if (i.target === d.target) {
             linkedByIndex[[d.source, i.source]] = 1;
           }
         });
-      });
+      }
+    });
 
-      return linkedByIndex;
-    }
+    return linkedByIndex;
+  }
 
-    let linkedByIndex = createLinkedByIndex(linkData);
+  let linkedByIndex = createLinkedByIndex(linkData);
 
-    function isConnected(a, b, links) {
-      return (
-        links[`${a.name},${b.name}`] ||
-        links[`${b.name},${a.name}`] ||
-        a.name === b.name
-      );
-    }
+  function isConnected(a, b, links) {
+    return (
+      links[`${a.name},${b.name}`] ||
+      links[`${b.name},${a.name}`] ||
+      a.name === b.name
+    );
+  }
 
-    select(node).attr("class", "bipartite");
+  const simulation = forceSimulation(nodes)
+    .force("link", forceLink(links).id(d => d.name))
+    .force("collide", forceCollide(15).iterations(16));
 
-    const simulation = forceSimulation(nodes)
-      .force("link", forceLink(links).id(d => d.name))
-      .force("collide", forceCollide(15).iterations(16))
-      .force(
-        "center",
-        forceCenter(this.props.size[0] / 2, this.props.size[1] / 2)
-      );
+  if (props.layout === "bipartite") {
+    simulation.force(
+      "center",
+      forceCenter(props.size[0] / 2, props.size[1] / 2)
+    );
+  } else {
+    simulation
+      .force("charge", forceManyBody().strength(-200))
+      .force("collide", forceCollide(32).iterations(16))
+      .force("X", forceX(props.size[0] / 2).strength(0.45))
+      .force("Y", forceY(props.size[1] / 2).strength(0.15));
+  }
 
-    const link = select(node)
-      .select("g.bipartite-links")
-      .selectAll(".bipartite line")
+  function restart() {
+    const svg = select(d3Container.current);
+    const orientation = props.orientation;
+    const displaySize = props.size;
+
+    const link = svg.select("g.bipartite-links").selectAll("line");
+
+    const circle = svg.select("g.bipartite-nodes").selectAll("g.circle");
+
+    link
       .data(links)
       .join("line")
       .attr("stroke", "black")
       .attr("stroke-opacity", 0.6);
-    const circle = select(node)
-      .select("g.bipartite-nodes")
-      .selectAll(".bipartite g.circle")
+
+    circle
       .data(nodes)
       .join("g")
       .attr("class", "circle");
@@ -86,45 +91,46 @@ class BipartiteGraph extends Component {
       .append("circle")
       .attr("stroke", "#fff")
       .attr("stroke-width", 1.5)
-      .attr("r", 10)
+      .attr("r", props.layout === "bipartite" ? 10 : 25)
       .attr("x", 0)
-      .attr("fill", d => (d.nodeLabel === "Person" ? "orange" : "lightblue"))
-      .each(
+      .attr("fill", d => (d.nodeLabel === "Person" ? "orange" : "lightblue"));
+
+    circle
+      .on("mouseover", highlightLinkedCircles)
+      .on("mouseout", unhighlightLinkedCircles);
+
+    if (props.layout === "bipartite") {
+      circle.each(
         d =>
           (d.fx =
             d.nodeLabel === "Person"
               ? displaySize[1] / 3
               : (displaySize[1] * 2) / 3)
-      )
-      .on("mouseover", highlightLinkedCircles)
-      .on("mouseout", unhighlightLinkedCircles);
+      );
 
-    circle
-      .append("text")
-      .text(d => d.name)
-      .attr("x", d => (d.nodeLabel === "Person" ? -13 : 13))
-      .attr("y", 3)
-      .attr("writing-mode", d => (orientation === "horizontal" ? "tb" : "lr"))
-      .attr("text-anchor", d => (d.nodeLabel === "Person" ? "end" : "start"));
+      circle
+        .append("text")
+        .text(d => d.name)
+        .attr("x", d => (d.nodeLabel === "Person" ? -13 : 13))
+        .attr("y", 3)
+        .attr("writing-mode", d => (orientation === "horizontal" ? "tb" : "lr"))
+        .attr("text-anchor", d => (d.nodeLabel === "Person" ? "end" : "start"));
+    } else {
+      var text = circle
+        .append("text")
+        .attr("y", d => (d.name.match(/\s/g) || []).length * -7.5 - 10);
 
-    function highlightLinkedCircles(d) {
-      linkedByIndex = createLinkedByIndex(linkData);
-      selectAll(".bipartite circle")
-        .filter(n => isConnected(d, n, linkedByIndex))
-        .transition()
-        .duration("500")
-        .attr("stroke", "#000")
-        .attr("stroke-width", 2.5)
-        .attr("r", 12);
-    }
-
-    function unhighlightLinkedCircles(d) {
-      selectAll(".bipartite circle")
-        .transition()
-        .duration("500")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-        .attr("r", 10);
+      text
+        .selectAll("tespan.text")
+        .data(d => d.name.split(" "))
+        .enter()
+        .append("tspan")
+        .attr("class", "text")
+        .text(d => d)
+        .attr("dy", 16)
+        .attr("x", 0)
+        .attr("dx", 0)
+        .attr("text-anchor", "middle");
     }
 
     simulation.on("tick", () => {
@@ -134,30 +140,51 @@ class BipartiteGraph extends Component {
         .attr("x2", d => d.target.x)
         .attr("y2", d => d.target.y);
 
-      circle
-        .each(
+      if (props.layout === "bipartite") {
+        circle.each(
           d =>
             (d.fx =
               d.nodeLabel === "Person"
                 ? displaySize[0] / 3
                 : (displaySize[0] * 2) / 3)
-        )
-        .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+        );
+      }
+      circle.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
     });
+
+    simulation
+      .nodes(nodes)
+      .force("link", forceLink(links).id(d => d.name))
+      .alpha(1)
+      .restart();
   }
 
-  render() {
-    return (
-      <svg
-        ref={node => (this.node = node)}
-        width={this.props.size[0]}
-        height={this.props.size[1]}
-      >
-        <g className="bipartite-links"></g>
-        <g className="bipartite-nodes"></g>
-      </svg>
-    );
+  function highlightLinkedCircles(d) {
+    linkedByIndex = createLinkedByIndex(linkData);
+    select(d3Container.current)
+      .selectAll("circle")
+      .filter(n => isConnected(d, n, linkedByIndex))
+      .transition()
+      .duration("500")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 2.5);
   }
+
+  function unhighlightLinkedCircles(d) {
+    select(d3Container.current)
+      .selectAll("circle")
+      .transition()
+      .duration("500")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5);
+  }
+
+  return (
+    <svg ref={d3Container} width={props.size[0]} height={props.size[1]}>
+      <g className="bipartite-links"></g>
+      <g className="bipartite-nodes"></g>
+    </svg>
+  );
 }
 
 export default BipartiteGraph;
--- ui/src/CommonTopicGraphs.js
+++ ui/src/CommonTopicGraphs.js
@@ -6,41 +6,49 @@ import ForceGraph from "./ForceGraph";
 class CommonTopicGraphs extends Component {
   constructor(props) {
     super(props);
-    this.createCommonTopicsGraphs = this.createCommonTopicsGraphs.bind(this);
   }
-
-  componentDidMount() {
-    this.createCommonTopicsGraphs();
-  }
-
-  componentDidUpdate() {
-    this.createCommonTopicsGraphs();
-  }
-
-  createCommonTopicsGraphs() {}
 
   render() {
+    const bipartite_nodes = this.props.bipartite_data.nodes;
+    const personNodes = [
+      ...new Set(bipartite_nodes.filter(d => d.nodeLabel === "Person"))
+    ];
+    const topicNodes = [
+      ...new Set(bipartite_nodes.filter(d => d.nodeLable !== "Person"))
+    ];
+    const longestNodeLength =
+      topicNodes.length > personNodes.length
+        ? topicNodes.length
+        : personNodes.length;
+    const displayHeight = longestNodeLength < 8 ? 160 : longestNodeLength * 22;
+    console.log(displayHeight);
+    console.log(longestNodeLength);
+    console.log(personNodes);
+    console.log(topicNodes);
+
     return (
       <div id="wrapper">
         <div id="personToPerson">
           <p>People with common interests</p>
-          <ForceGraph
+          <BipartiteGraph
             data={this.props.p2p_data}
-            size={[360, this.props.bipartite_data.links.length * 20]}
+            size={[360, displayHeight]}
+            layout={"force"}
           />
         </div>
         <div id="personToTopic">
           <BipartiteGraph
             data={this.props.bipartite_data}
-            size={[360, this.props.bipartite_data.links.length * 20]}
-            orientation={"vertical"}
+            size={[360, displayHeight]}
+            layout={"bipartite"}
           />
         </div>
         <div id="topicToTopic">
           <p>Topics with common people</p>
-          <ForceGraph
+          <BipartiteGraph
             data={this.props.t2t_data}
-            size={[360, this.props.bipartite_data.links.length * 20]}
+            size={[360, displayHeight]}
+            layout={"force"}
           />
         </div>
       </div>

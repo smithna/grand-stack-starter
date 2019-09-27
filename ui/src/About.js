34e3bf7c5bee4d84c456327ac42b20fcf0fd2c61
sarah090 <55504405+sarah090@users.noreply.github.com>
2019-09-27 08:07:50 -0500

    new about page

---
+++ ui/src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return <p>
+    1.	A note that this project was built for the 2019 Global Graph Hack with a link to the hackathon page so people could check out other projects
+    2.	A link to the GitHub repository so they can see the source code
+    3.	A link to set up a Neo4j sandbox
+    4.	A link to the Grand Stack docs page
+    5.	A link to OneGraph so they could see how to use GraphQL to easily authenticate and pull data from social and business sites
+    </p>;
+  }
+}
+
+export default About;
